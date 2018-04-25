80f3879984ca1b9e54ddf55f348d3113b7e5683a
yangbinnnn <yangbinnnn@gmail.com>
2018-04-25 23:09:46 +0800

    update

---
+++ levels/05_ssh_tunnel/ssh_tunnel.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+)
+
+type Endpoint struct {
+	Host string
+	Port int
+}
+
+func (endpoint *Endpoint) String() string {
+	return fmt.Sprintf("%s:%d", endpoint.Host, endpoint.Port)
+}
+
+type SSHtunnel struct {
+	Local  *Endpoint
+	Server *Endpoint
+	Remote *Endpoint
+
+	Config *ssh.ClientConfig
+}
+
+func (tunnel *SSHtunnel) Start() error {
+	listener, err := net.Listen("tcp", tunnel.Local.String())
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+		go tunnel.forward(conn)
+	}
+}
+
+func (tunnel *SSHtunnel) forward(localConn net.Conn) {
+	serverConn, err := ssh.Dial("tcp", tunnel.Server.String(), tunnel.Config)
+	if err != nil {
+		fmt.Printf("Server dial error: %s\n", err)
+		return
+	}
+
+	remoteConn, err := serverConn.Dial("tcp", tunnel.Remote.String())
+	if err != nil {
+		fmt.Printf("Remote dial error: %s\n", err)
+		return
+	}
+
+	copyConn := func(writer, reader net.Conn) {
+		_, err := io.Copy(writer, reader)
+		if err != nil {
+			fmt.Printf("io.Copy error: %s", err)
+		}
+	}
+
+	go copyConn(localConn, remoteConn)
+	go copyConn(remoteConn, localConn)
+}
+
+func SSHAgent() ssh.AuthMethod {
+	if sshAgent, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
+		return ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers)
+	}
+	return nil
+}
+
+func publicKeyFile(file string) ssh.AuthMethod {
+	buffer, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatalln(fmt.Sprintf("Cannot read SSH public key file %s", file))
+		return nil
+	}
+
+	key, err := ssh.ParsePrivateKey(buffer)
+	if err != nil {
+		log.Fatalln(fmt.Sprintf("Cannot parse SSH public key file %s", file))
+		return nil
+	}
+	return ssh.PublicKeys(key)
+}
+
+func main() {
+	localEndpoint := &Endpoint{
+		Host: "localhost",
+		Port: 9000,
+	}
+
+	serverEndpoint := &Endpoint{
+		Host: "61.155.138.173",
+		Port: 11422,
+	}
+
+	remoteEndpoint := &Endpoint{
+		Host: "localhost",
+		Port: 8888,
+	}
+
+	sshConfig := &ssh.ClientConfig{
+		User: "root",
+		Auth: []ssh.AuthMethod{
+			// SSHAgent(),
+			publicKeyFile("/Users/yang/.ssh/id_rsa"),
+		},
+	}
+
+	tunnel := &SSHtunnel{
+		Config: sshConfig,
+		Local:  localEndpoint,
+		Server: serverEndpoint,
+		Remote: remoteEndpoint,
+	}
+
+	tunnel.Start()
+}
